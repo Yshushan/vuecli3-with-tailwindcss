bec51e6e370f3d6f33da5b1f168bb3d3ca5b48fb
yangshushan <yss_2016@outlook.com>
2019-09-05 21:23:44 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+    <button class="uppercase mt-10 mx-auto active:bg-blue-700 focus:outline-none">click me</button>
+    <div class="content container mx-auto shadow-2xl">
+      <h2 class="capitalize">this is title</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ducimus eveniet aspernatur veniam et magni repudiandae libero. Consequatur nisi asperiores possimus voluptate sint officiis, corporis accusamus aperiam molestiae veritatis natus.</p>
+      <a href="#" class="text-gray-600 hover:text-gray-300 hover:underline">This is a link</a>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+<style lang="scss">
+$white: white;
+.content{
+  padding: 2rem 4rem;
+  background-color: rgb(3, 20, 44);
+  h2{
+    color: $white;
+    font-size: 1.5rem;
+  }
+  p{
+    color: $white;
+  }
+}
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
-};
-</script>
+</style>
